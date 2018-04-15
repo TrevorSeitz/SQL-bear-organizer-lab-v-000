eab5f03d81a8d12fc64210c7851f60a37733c32e
Trevor Seitz <trevinla@gmail.com>
2018-04-15 18:40:09 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Mr. Chocolate);
 INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Rowdy);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ();
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Tabitha);
 INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ();
 INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ();
 INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES ();
