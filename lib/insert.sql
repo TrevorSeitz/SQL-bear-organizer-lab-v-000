c5fc320aeb2257e2edc1c762dc7a542259831827
Trevor Seitz <trevinla@gmail.com>
2018-04-15 18:44:20 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Mr. Chocolate, 5, m, black, good, y);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Rowdy);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Tabitha);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Sergeant Brown);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Melissa);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Grinch);
-INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Wendy);
-INSERT INTO bears (age, gender, color, temperment, alive) VALUES ();
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Rowdy, 3, m, brown, good, y);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Tabitha, 6, m, black, ok, y);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Sergeant Brown, 5, m, brown, good, y);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Melissa), 5, m, black, good, y;
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Grinch, 5, m, black, good, y);
+INSERT INTO bears (name, age, gender, color, temperment, alive) VALUES (Wendy, 5, m, black, good, y);
+INSERT INTO bears (age, gender, color, temperment, alive) VALUES (, 5, m, black, good, y);
